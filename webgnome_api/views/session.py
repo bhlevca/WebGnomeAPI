b2905a0fd7db88ff11929da75d28f1643b7a0eac
James Makela <james.makela@noaa.gov>
2014-06-03 11:23:02 -0700

    serialize the acquisition of a session

--- webgnome_api/views/session.py
+++ webgnome_api/views/session.py
@@ -5,11 +5,17 @@ from webgnome_api.common.views import (cors_policy)
 from cornice import Service
 
 
-session = Service(name='session', path='/session', description="Session managment", cors_policy=cors_policy)
+session = Service(name='session', path='/session',
+                  description="Session managment", cors_policy=cors_policy)
 
 
 @session.post()
 def get_info(request):
+    gnome_sema = request.registry.settings['py_gnome_semaphore']
+    gnome_sema.acquire()
+
     request.session.changed()
+
+    gnome_sema.release()
 
     return {'id': request.cookies['session']}
