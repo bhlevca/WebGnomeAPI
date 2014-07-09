e6e93b18475fba70c6d5c5ca56ee86584bec4dc7
James Makela <james.makela@noaa.gov>
2014-07-09 15:47:54 -0700

    Added NetCDFOutput object API

--- webgnome_api/tests/test_outputter.py
+++ webgnome_api/tests/test_outputter.py
@@ -145,3 +145,32 @@ class RendererTests(OutputterTests):
 
         assert json_obj['viewport'] == [[-100.0, 100.0],
                                         [-100.0, 100.0]]
+
+
+class NetCDFOutputterTests(OutputterTests):
+    '''
+        Tests out the Gnome NetCDFOutput object API
+    '''
+    req_data = {'obj_type': u'gnome.outputters.netcdf.NetCDFOutput',
+                'id': u'5e1fb440-07b9-11e4-b447-3c075404121a',
+                'json_': u'webapi',
+                'name': u'sample_model.nc',
+                'netcdf_filename': u'sample_model.nc',
+                'compress': True,
+                'output_last_step': True,
+                'output_zero_step': True}
+
+    def check_created_values(self, json_obj1, json_obj2):
+        for k in ('name', 'netcdf_filename',
+                  'compress', 'output_last_step', 'output_zero_step'):
+            assert json_obj1[k] == json_obj2[k]
+
+    def perform_updates(self, json_obj):
+        json_obj['output_last_step'] = False
+        json_obj['output_zero_step'] = False
+        json_obj['compress'] = False
+
+    def check_updates(self, json_obj):
+        assert json_obj['output_last_step'] == False
+        assert json_obj['output_zero_step'] == False
+        assert json_obj['compress'] == False
--- webgnome_api/views/outputter.py
+++ webgnome_api/views/outputter.py
@@ -11,10 +11,9 @@ from cornice import Service
 outputter = Service(name='outputter', path='/outputter*obj_id',
                   description="Outputter API", cors_policy=cors_policy)
 
-implemented_types = (
-                     'gnome.outputters.outputter.Outputter',
+implemented_types = ('gnome.outputters.outputter.Outputter',
                      'gnome.outputters.renderer.Renderer',
-                     )
+                     'gnome.outputters.netcdf.NetCDFOutput')
 
 
 @outputter.get()
