cc88efc1a748638dab306d6c975c2deb9b6f358e
James Makela <james.makela@noaa.gov>
2014-08-06 08:30:13 -0700

    Setting up a —skipslow option for py.test (We want to run all tests by default). Adding a unit test area for testing full-run scenarios.

--- webgnome_api/tests/test_model_runs.py
+++ webgnome_api/tests/test_model_runs.py
@@ -4,14 +4,17 @@ Functional tests for the Gnome Location object Web API
 from pprint import PrettyPrinter
 pp = PrettyPrinter(indent=2)
 
+import os
+import time
+
 import pytest
 
 from base import FunctionalTestBase
 
 
-class StepTest(FunctionalTestBase):
+class ModelRunTest(FunctionalTestBase):
     '''
-        Tests out the Gnome Location object API
+        Tests out a Model run with the WebGnome API
     '''
     spill_data = {'obj_type': 'gnome.spill.spill.Spill',
                   'name': 'What a Name',
@@ -19,12 +22,12 @@ class StepTest(FunctionalTestBase):
                   'release': {'obj_type': ('gnome.spill.release'
                                            '.PointLineRelease'),
                               'num_elements': 1000,
-                              'num_released': 84,
-                              'release_time': '2013-02-13T09:00:00',
-                              'end_release_time': '2013-02-13T15:00:00',
+                              'num_released': 0,
+                              'release_time': '2014-08-06T08:00:00',
+                              'end_release_time': '2014-08-06T08:00:00',
                               'start_time_invalid': False,
-                              'end_position': [144.664166, 13.441944, 0.0],
-                              'start_position': [144.664166, 13.441944, 0.0],
+                              'end_position': [-72.419992, 41.202120, 0.0],
+                              'start_position': [-72.419992, 41.202120, 0.0]
                               },
                   'element_type': {'obj_type': ('gnome.spill.elements'
                                                 '.ElementType'),
@@ -53,56 +56,8 @@ class StepTest(FunctionalTestBase):
                     'output_dir': 'models/images'
                     }
 
-    def test_first_step(self):
-        # We are testing our ability to generate the first step in a model run
-        resp = self.testapp.get('/location/central-long-island-sound')
-
-        assert 'name' in resp.json_body
-        assert 'steps' in resp.json_body
-        assert 'geometry' in resp.json_body
-        assert 'coordinates' in resp.json_body['geometry']
-
-        # OK, if we get this far, we should have an active model
-        print 'test_first_step(): getting model...'
-        resp = self.testapp.get('/model')
-        model1 = resp.json_body
-
-        # The location file we selected should have:
-        # - a registered map
-        # - a registered Tide
-        # - a registered RandomMover
-        # - a registered CatsMover
-
-        # so what do we still need?
-        # - maybe a wind and a windmover??? (optional)
-
-        # - we need a spill
-        print 'test_first_step(): creating spill...'
-        resp = self.testapp.post_json('/spill', params=self.spill_data)
-        spill = resp.json_body
-        model1['spills'] = [spill]
-
-        # - we need an outputter
-        print 'test_first_step(): creating outputter...'
-        resp = self.testapp.post_json('/outputter', params=self.geojson_data)
-        outputter = resp.json_body
-        model1['outputters'] = [outputter]
-
-        resp = self.testapp.put_json('/model', params=model1)
-        model1 = resp.json_body
-
-        assert model1['spills'][0]['id'] == spill['id']
-        assert model1['outputters'][0]['id'] == outputter['id']
-
-        # Alright, now we can try a first step.
-        # This does not(should not?) require a step_id
-        resp = self.testapp.get('/step')
-        first_step = resp.json_body
-
-        assert first_step['GeoJson']['step_num'] == 0
-
     @pytest.mark.slow
-    def test_all_steps(self):
+    def test_full_run(self):
         # We are testing our ability to generate the first step in a model run
         resp = self.testapp.get('/location/central-long-island-sound')
 
@@ -117,6 +72,8 @@ class StepTest(FunctionalTestBase):
         model1 = resp.json_body
         num_time_steps = model1['num_time_steps']
 
+        print 'Our model:'
+        pp.pprint(model1)
         # The location file we selected should have:
         # - a registered map
         # - a registered Tide
@@ -125,6 +82,10 @@ class StepTest(FunctionalTestBase):
 
         # so what do we still need?
         # - maybe a wind and a windmover??? (optional)
+
+        #print 'our map:'
+        #pp.pprint(model1['map'])
+        #raise
 
         # - we need a spill
         print 'test_all_steps(): creating spill...'
@@ -153,8 +114,12 @@ class StepTest(FunctionalTestBase):
             step = resp.json_body
             print '{0}, '.format(step['GeoJson']['step_num']),
             assert step['GeoJson']['step_num'] == s
+            assert os.path.exists(step['GeoJson']['output_filename'])
 
         # an additional call to /step should generate a 404
         resp = self.testapp.get('/step', status=404)
 
+        open('models/images/done', 'w').write('done!\n')
+        time.sleep(4)
         print 'done!'
+        raise
