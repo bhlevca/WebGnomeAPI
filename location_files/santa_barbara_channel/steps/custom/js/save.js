35c36ebf0fc6d974e23a3209a70ea35fef42ef77
Alexander Miranda <alex.miranda@noaa.gov>
2015-11-18 09:52:52 -0800

    Removed link tags for the custom step templates

--- location_files/santa_barbara_channel/steps/custom/js/save.js
+++ location_files/santa_barbara_channel/steps/custom/js/save.js
@@ -1,3 +1,15 @@
 (function(form){
-   console.log(form);
+   var optionsArr = $('.option', form.html());
+   var movers = webgnome.model.get('movers');
+   for (var i = 0; i < optionsArr.length; i++) {
+      var currentOption = $(optionsArr[i]);
+      if (currentOption.hasClass('upwelling')) {
+         movers.findWhere({'name': 'Upwelling.cur'}).set('on', optionsArr[i].dataset.clicked);
+      } else if (currentOption.hasClass('convergent')) {
+         movers.findWhere({'name': 'Convergent.cur'}).set('on', optionsArr[i].dataset.clicked);
+      } else if (currentOption.hasClass('relaxation')) {
+         movers.findWhere({'name': 'Relaxation.cur'}).set('on', optionsArr[i].dataset.clicked);
+      }
+   }
+   webgnome.model.save();
 }(form));
--- location_files/santa_barbara_channel/steps/custom/js/setup.js
+++ location_files/santa_barbara_channel/steps/custom/js/setup.js
@@ -1,7 +1,7 @@
 (function(modal, modaljq){
+	$('.option', modal).attr('data-clicked', false);
 	var thumbnailHighlight = function(e) {
-       $(e.target, modal).closest('.option').addClass('selected');
-       $(modal).trigger('save');
+       $(e.target, modal).closest('.option').attr('data-clicked', true);
 	};
 
 	$('.option', modal).on('click', thumbnailHighlight);
