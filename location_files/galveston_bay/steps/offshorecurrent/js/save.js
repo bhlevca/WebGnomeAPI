693c2415f9f1baa3ac406f71720fa3a8fe1d0c66
Alexander Miranda <alex.miranda@noaa.gov>
2015-12-02 12:52:41 -0800

    Added save script for the offshorecurrent custom step

---
+++ location_files/galveston_bay/steps/offshorecurrent/js/save.js
@@ -0,0 +1,33 @@
+(function (form){
+    var selector = form.selector;
+    var speed = $(selector + ' #speed').val();
+    var speed_units = $(selector + ' #speed-units').val();
+    var direction = $(selector + ' #direction').val();
+    var direction_units = $(selector + ' #direction-units').val();
+    var Offshore_Mover = webgnome.model.get('movers').findWhere({'name': 'Offshore.cur'});
+    var cat_v_at_ref = 0.247137;
+    var cat_v_at_B = 0.015001;
+
+    if (speed_units !== 'm/s') {
+        if (speed_units === 'cm/s') {
+            speed /= 100;
+        } else if (speed_units === 'knots') {
+            speed *= 0.514444;
+        }
+    }
+
+    if (direction_units === 'rad') {
+        direction *= (180 / Math.PI);
+    }
+
+    direction -= 55;
+
+    direction *= (Math.PI / 180);
+
+    var cos_rad = Math.cos(direction);
+    var alongshelf = speed * cos_rad;
+    var alongshelf_ref = (-1 * alongshelf) * (cat_v_at_ref / cat_v_at_B);
+
+    Offshore_Mover.set('scale_value', alongshelf_ref);
+    webgnome.model.save();
+}(form));
