5c9a2703c14836ecd4700d296044346f22e96732
Alexander Miranda <alex.miranda@noaa.gov>
2015-12-03 11:05:45 -0800

    Updated the north slope custom step' scripts and templates

--- location_files/north_slope/steps/colvillekuparukflow/js/save.js
+++ location_files/north_slope/steps/colvillekuparukflow/js/save.js
@@ -1,16 +1,37 @@
 (function(form){
-	var selector = form.selector;
+    var selector = form.selector;
     
     function colvilleRiver() {
         var colvilleFlow = $(selector + ' #colville-flow').val();
         var colvilleMover = webgnome.model.get('movers').findWhere({'name': 'ColvilleRiver.cur'});
+        var COLLVILLE_SCALE = 0.183 / 1500;
+        var colvilleScaled = colvilleFlow * COLLVILLE_SCALE;
+
+        colvilleMover.set('scale_value', colvilleScaled);
     }
 
     function kuparukRiver() {
+        var kuparukFlow = $(selector + ' #kuparuk-flow').val();
+        var kuparukMover = webgnome.model.get('movers').findWhere({'name': 'KuparukRiver.cur'});
+        var KUPARUK_SCALE = 0.1814 / 957;
 
+        if (kuparukFlow === 'other') {
+           var kuparukVal = parseFloat($(selector + ' #kuparuk-flow-manual').val());
+           if ($(selector + ' #kuparuk-flow-manual-units').val() === 'kcfs') {
+                kuparukVal *= 1000;
+           }
+           kuparukFlow = kuparukVal;
+        } else {
+            kuparukFlow = parseFloat(kuparukFlow);
+        }
+
+        var kuparukScaled = kuparukFlow * KUPARUK_SCALE;
+
+        kuparukMover.set('scale_value', kuparukScaled);
     }
 
     colvilleRiver();
     kuparukRiver();
+
     webgnome.model.save();
 }(form));
