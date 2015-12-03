b9178a8e4a0c28b463fc5214199cc2fdc33cc8b4
Alexander Miranda <alex.miranda@noaa.gov>
2015-12-03 12:58:33 -0800

    Updated the lower mississippi location file template and save script for the custom step

---
+++ location_files/lower_mississippi/steps/custom/js/save.js
@@ -0,0 +1,34 @@
+(function(form){
+	var selector = form.selector;
+	var datatype = $(selector + ' #datatype').val();
+    var missRiverMover = webgnome.model.get('movers').findWhere({'name': 'MissRiver.cur'});
+    var stageHeight = $(selector + ' #stageheight').val();
+    var speed = parseFloat($(selector + ' #currentspeed').val());
+    var speedms;
+
+	if (datatype === 'height'){
+        var heightUnits = $(selector + ' #stageheight-units').val();
+
+        if (heightUnits === 'm') {
+            stageHeight *= 3.28084;
+        }
+
+        speedms = (0.0011 * Math.pow(stageHeight, 2) + 0.15 * stageHeight + 0.3868) * 0.5144;
+
+        if (stageHeight <= 8) {
+            speedms *= 1.5;
+        }
+    } else {
+        if ($(selector + ' #currentspeed-units').val() !== 'm/s') {
+            if ($(selector + ' #currentspeed-units').val() === 'knots') {
+                speed *= 0.5144;
+            } else {
+                speed *= 0.01;
+            }
+        }
+        speedms = speed;
+	}
+
+    missRiverMover.set('scale_value', speedms);
+    webgnome.model.save();
+}(form));
