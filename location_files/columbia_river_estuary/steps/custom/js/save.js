ad03fcdcb5ac19e97a30484cd4fbc59f0a70749a
Alexander Miranda <alex.miranda@noaa.gov>
2015-12-07 09:42:11 -0800

    Updated the Columbia river custom step's save script and template

---
+++ location_files/columbia_river_estuary/steps/custom/js/save.js
@@ -0,0 +1,45 @@
+(function (form){
+	var selector = form.selector;
+	var flow = $(selector + ' #flow-rate').val();
+	var riverMover = webgnome.model.get('movers').findWhere({'name': 'RiverFlow.cur'});
+	var scale = 0.2725 / 223.027;
+	var v_scale, tongue_point;
+
+	var convertToKCFS = function(flow, units){
+		if (units === 'cfs') {
+			flow /= 1000;
+		} else if (units === 'm3/s') {
+			flow *= 35.3146667;
+			flow /= 1000;
+		}
+		return flow;
+	};
+
+	if (flow === 'input') {
+		var bonneFlow = parseFloat($(selector + ' #bonne-flow').val());
+		var willFlow = parseFloat($(selector + ' #william-flow').val());
+		var units = {};
+		units['bonne'] = $(selector + ' #bonne-flow-units').val();
+		units['will'] = $(selector + ' #william-flow-units').val();
+		var transport;
+
+		bonneFlow = convertToKCFS(bonneFlow, units.bonne);
+		willFlow = convertToKCFS(willFlow, units.will);
+
+		if ((bonneFlow <= 200) && (willFlow <= 90)) {
+			transport = (4.139 + (1.003 * bonneFlow)) + (1.632 * willFlow);
+		} else {
+			transport = (103 + (1.084 * bonneFlow)) + (1.757 * willFlow);
+		}
+
+		tongue_point = scale * transport;
+		v_scale = tongue_point - 0.200;
+	} else {
+		var flowNum = parseFloat(flow);
+		tongue_point = scale * flowNum;
+		v_scale = tongue_point - 0.200;
+	}
+
+	riverMover.set('scale_value', v_scale);
+	webgnome.model.save();
+}(form));
