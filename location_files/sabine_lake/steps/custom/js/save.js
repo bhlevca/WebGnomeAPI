ffdbf8a95e11634dcda4cf0b40ffbc9b4c609cb9
Alexander Miranda <alex.miranda@noaa.gov>
2016-02-01 10:42:13 -0800

    Updated the validation messages of the Sabine Lake location to be more descriptive

--- location_files/sabine_lake/steps/custom/js/save.js
+++ location_files/sabine_lake/steps/custom/js/save.js
@@ -2,23 +2,32 @@
     var selector = form.selector;
     var sabineRiver = webgnome.model.get('movers').findWhere({'name': 'SabineRiver.cur'});
     var nechesRiver = webgnome.model.get('movers').findWhere({'name': 'NechesRiver.cur'});
-    var sab_flow, nec_flow, sab_scale, nec_scale;
+    var sab_flow, nec_flow, sab_scale, nec_scale, sab_units, nec_units, errMsg;
 
     if ($(selector + ' #sabine-flowrate').val() !== 'other') {
         sab_flow = parseFloat($(selector + ' #sabine-flowrate').val());
     } else {
         sab_flow = parseFloat($(selector + ' #sabine-flowrate-manual').val());
+        sab_units = $(selector + ' #sabine-flowrate-units').val();
 
         if (isNaN(sab_flow)) {
             return "Please enter a number for Sabine flow rate!";
         }
 
-        if ($(selector + ' #sabine-flowrate-units').val() !== 'cfs') {
+        if (sab_units !== 'cfs') {
             sab_flow *= 1000;
         }
 
         if (sab_flow > 20000 || sab_flow < 10) {
-            return "The entered Sabine flow rate is outside the acceptable range!";
+            errMsg = "The entered Sabine flow rate is outside the acceptable range of ";
+
+            if (sab_units === 'cfs') {
+                errMsg += "10,000 and 20,000,000 cfs!";
+            } else if (sab_units === 'kcfs') {
+                errMsg += "10 and 20,000 kcfs!";
+            }
+
+            return errMsg;
         }
     }
 
@@ -26,17 +35,26 @@
         nec_flow = parseFloat($(selector + ' #neches-flowrate').val());
     } else {
         nec_flow = parseFloat($(selector + ' #neches-flowrate-manual').val());
+        nec_units = $(selector + ' #neches-flowrate-units').val();
 
         if (isNaN(nec_flow)) {
             return "Please enter a number for Neches flow rate!";
         }
 
-        if ($(selector + ' #neches-flowrate-units').val() !== 'cfs') {
+        if (nec_units !== 'cfs') {
             nec_flow *= 1000;
         }
 
         if (nec_flow > 3800 || nec_flow < 1) {
-            return "The entered Neches flow rate is outside the acceptable range!";
+            errMsg = "The entered Neches flow rate is outside the acceptable range of ";
+            
+            if (nec_units === 'cfs') {
+                errMsg += "1,000 and 3,800,000 cfs!";
+            } else if (nec_units === 'kcfs') {
+                errMsg += "1 and 3,800 kcfs!";
+            }
+
+            return errMsg;
         }
     }
 
