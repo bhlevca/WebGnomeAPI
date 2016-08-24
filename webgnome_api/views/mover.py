42f8c1ff7183c10afff699ea0b7fdd9febdfb2a5
James L. Makela <james.makela@noaa.gov>
2016-08-24 14:40:46 -0700

    The new Grid movers return a simple, mostly flattened, list of grid data when requested. So the mover needed some updates as well as the mover unit tests.

--- webgnome_api/views/mover.py
+++ webgnome_api/views/mover.py
@@ -4,17 +4,13 @@ This currently includes ??? objects.
 """
 import logging
 import ujson
-import os
 import numpy as np
-
-from geojson import Feature, FeatureCollection, MultiPolygon, Polygon
 
 from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound
 from cornice import Service
 
-from gnome import basic_types
 from gnome.movers.current_movers import CurrentMoversBase
 
 from webgnome_api.common.views import (get_object,
@@ -52,7 +48,7 @@ def get_mover(request):
     if (len(content_requested) > 1 and
             content_requested[1] == 'grid'):
         return get_current_info(request)
-    elif (len(content_requested) > 1 and 
+    elif (len(content_requested) > 1 and
             content_requested[1] == 'centers'):
         return get_grid_centers(request)
     else:
@@ -70,9 +66,11 @@ def update_mover(request):
     '''Updates a Mover object.'''
     return update_object(request, implemented_types)
 
+
 @view_config(route_name='mover_upload', request_method='OPTIONS')
 def mover_upload_options(request):
     return cors_response(request, request.response)
+
 
 @view_config(route_name='mover_upload', request_method='POST')
 def upload_mover(request):
@@ -80,6 +78,7 @@ def upload_mover(request):
     resp = Response(ujson.dumps({'filename': file_path}))
 
     return cors_response(request, resp)
+
 
 def get_current_info(request):
     '''
@@ -99,17 +98,8 @@ def get_current_info(request):
         obj_id = request.matchdict.get('obj_id')[0]
         mover = get_session_object(obj_id, request)
 
-        if mover is not None:
-            # start = active_model.start_time
-            # start_seconds = time_utils.date_to_sec(start)
-            # num_hours = active_model.duration.total_seconds() / 60 / 60
-            #
-            # times = [start_seconds + 3600. * dt
-            #          for dt in range(int(num_hours))]
-
-            if isinstance(mover, CurrentMoversBase):
-                # signature = get_grid_signature(mover)
-                cells = get_cells(mover)
+        if mover is not None and isinstance(mover, CurrentMoversBase):
+            cells = get_cells(mover)
 
             return cells.reshape(-1, cells.shape[-1]*cells.shape[-2]).tolist()
         else:
@@ -120,6 +110,7 @@ def get_current_info(request):
         log.info('  ' + log_prefix + 'semaphore released...')
 
     log.info('<<' + log_prefix)
+
 
 def get_grid_centers(request):
     '''
@@ -136,11 +127,8 @@ def get_grid_centers(request):
         obj_id = request.matchdict.get('obj_id')[0]
         mover = get_session_object(obj_id, request)
 
-        if mover is not None:
-            
-            if isinstance(mover, CurrentMoversBase):
-                # signature = get_grid_signature(mover)
-                centers = get_center_points(mover)
+        if mover is not None and isinstance(mover, CurrentMoversBase):
+            centers = get_center_points(mover)
 
             return centers.tolist()
         else:
