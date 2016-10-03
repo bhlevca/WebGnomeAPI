6504076cd81e8762a2cf698aadff1f6aa76ad823
Alexander Miranda <alex.miranda@noaa.gov>
2016-10-03 12:05:24 -0700

    Added Content-Disposition header to the FileResponse in the case of zipped up outputter files so the downloaded file has a .zip extension

--- webgnome_api/views/export.py
+++ webgnome_api/views/export.py
@@ -4,18 +4,20 @@ Views for file download operations.
 import os
 import zipfile
 
+from uuid import uuid4
+
 from pyramid.view import view_config
 from pyramid.response import FileResponse
 from pyramid.httpexceptions import HTTPNotFound
 
-from webgnome_api.common.common_object import get_session_dir, obj_id_from_url
+from webgnome_api.common.common_object import get_session_dir
 from webgnome_api.common.session_management import get_active_model
 from webgnome_api.common.views import cors_response
 
 
 @view_config(route_name='export', request_method='GET')
 def download_file(request):
-    obj_id = obj_id_from_url(request)
+    obj_id = uuid4()
     session_path = get_session_dir(request)
     file_path = os.path.sep.join(map(str, request.matchdict['file_path']))
     output_path = os.path.join(session_path, file_path)
@@ -35,7 +37,9 @@ def download_file(request):
                     zipfile_path = os.path.join(dirname, filename)
                     zf.write(zipfile_path, os.path.basename(zipfile_path))
         zf.close()
-        return FileResponse(zip_path, request)
+        response = FileResponse(zip_path, request)
+        response.headers['Content-Disposition'] = ("attachment; filename={0}".format(os.path.basename(zip_path)))
+        return response
     elif os.path.isfile(output_path):
         return FileResponse(output_path, request)
     else:
