7b41c43bc33cff0ebf28be32c6b43a9272ab5a90
James L. Makela <james.makela@noaa.gov>
2017-10-29 12:51:42 -0700

    Added ability to create upload sub-folders and rename uploaded files.

--- webgnome_api/views/upload_manager.py
+++ webgnome_api/views/upload_manager.py
@@ -2,16 +2,24 @@
 Views for the model load/save operations.
 """
 import os
+import shutil
 import errno
 import logging
 import urllib
+import ujson
 
 from pyramid.httpexceptions import (HTTPNotFound,
-                                    HTTPUnauthorized)
+                                    HTTPBadRequest,
+                                    HTTPUnauthorized,
+                                    HTTPInternalServerError)
 
 from cornice import Service
 
-from ..common.system_resources import list_files
+from ..common.helpers import PyObjFromJson
+from ..common.system_resources import (list_files,
+                                       file_info,
+                                       mkdir,
+                                       rename_or_move)
 from ..common.common_object import (get_persistent_dir)
 from ..common.views import (can_persist,
                             cors_exception,
@@ -51,6 +59,76 @@ def get_uploaded_files(request):
             raise cors_exception(request, HTTPUnauthorized)
         else:
             raise
+
+
+@upload_manager.post()
+@can_persist
+def modify_filesystem(request):
+    '''
+        Make a file system modification within the uploads folder.
+        Currently, we support the following actions:
+        - create a new directory
+        - rename a file
+        - move a file into a directory (similar to renaming)
+    '''
+    sub_folders = [urllib.unquote(d).encode('utf8')
+                   for d in request.matchdict['sub_folders']
+                   if d != '..']
+
+    requested_path = os.path.join(get_persistent_dir(request), *sub_folders)
+
+    try:
+        file_model = PyObjFromJson(ujson.loads(request.body))
+    except Exception:
+        raise cors_exception(request, HTTPBadRequest)
+
+    # log.info('requested_path: {}'.format(requested_path))
+    # log.info('name: {}'.format(file_model.name))
+    # log.info('size: {}'.format(file_model.size))
+    # log.info('type: {}'.format(file_model.type))
+
+    if (file_model.type == 'd'):
+        return create_new_folder(request, requested_path, file_model)
+    else:
+        return rename_file(request, requested_path, file_model)
+
+
+def create_new_folder(request, requested_path, file_model):
+    '''
+        Create a new folder within the uploads folder.
+    '''
+    log.info('creating a new folder: {}'.format(file_model.name))
+    try:
+        mkdir(requested_path, file_model.name)
+    except OSError:
+        raise cors_exception(request, HTTPInternalServerError)
+
+    return file_info(requested_path, file_model.name)
+
+
+def rename_file(request, requested_path, file_model):
+    '''
+        Rename a file within the uploads folder.
+    '''
+    log.info('renaming file from {} to {}'.format(file_model.name,
+                                                  file_model.new_name))
+    try:
+        old_name = os.path.basename(file_model.name)
+        new_name = os.path.basename(file_model.new_name)
+
+        rename_or_move(requested_path, old_name, new_name)
+    except Exception:
+        raise cors_exception(request, HTTPInternalServerError)
+
+    # Backbone.js likes to sync its models with the REST services it
+    # communicates with.  So we need to return a model object that agrees
+    # with what it expects when it performs a model.save() operation.
+    # Otherwise, Backbone will treat it as a server-side change and perform
+    # additional, and unwanted, requests.
+    return {'name': file_model.name,
+            'size': file_model.size,
+            'type': file_model.type}
+
 
 
 
