bc6d61511345e48dc07cce3c09f0a0a8acbfd732
jay.hennen <jay.hennen@noaa.gov>
2018-06-28 17:08:33 -0700

    logger should now be instanced properly
    Each session attaches a handler to the overall logger that processes
    messages for that session only. Each session now also stores it's log in
    the folder associated with that session.

--- webgnome_api/views/socket_logger.py
+++ webgnome_api/views/socket_logger.py
@@ -7,35 +7,65 @@ from pygtail import Pygtail
 import gevent
 
 from socketio.namespace import BaseNamespace
-
+import logging
+import os
+import pdb
 
 class LoggerNamespace(BaseNamespace):
     def recv_connect(self):
         print "CONN LOGGER"
         self.emit("connected")
 
-        def send_logs():
-            hasher = hashlib.sha1(self.request.session.session_id)
-            session_hash = base64.urlsafe_b64encode(hasher.digest())
+        overall_logger = logging.root
+        formatter = overall_logger.handlers[0].formatter
+        pattern = re.compile('^(?P<date>.*?)\s+'
+                             '(?P<time>.*?)\s+'
+                             '(?P<level>.*?)\s+'
+                             '(?P<session_hash>.*?)\s+'
+                             '(?P<name>.*?)\s+'
+                             '(?P<message>.*?)$')
 
-            pattern = re.compile('^(?P<date>.*?)\s+'
-                                 '(?P<time>.*?)\s+'
-                                 '(?P<level>.*?)\s+'
-                                 '(?P<session_hash>.*?)\s+'
-                                 '(?P<name>.*?)\s+'
-                                 '(?P<message>.*?)$')
+        overall_logger.info('{0} handlers attached'.format(len(overall_logger.handlers)))
+        overall_logger.info('{0}'.format(self.socket))
+        if len(overall_logger.handlers) > 50:
+            #To stop the logger root from getting drowned in handlers
+            del overall_logger.handlers[2];
 
-            while True:
-                for line in Pygtail('messages.log'):
-                    if line.find(session_hash) >= 0:
-                        msg_obj = pattern.match(line).groupdict()
-                        del msg_obj['session_hash']
+        existing_handler=None
+        for i, handler in enumerate(overall_logger.handlers):
+            if (hasattr(handler, 'session_id') and handler.session_id == self.request.session.session_id):
+                overall_logger.info('existing handler for the session {0} found'.format(self.request.session.session_id))
+                existing_handler = overall_logger.handlers[i]
 
-                        self.emit('log', msg_obj)
+        def emit_msg(logrecord):
+            msg_obj = pattern.match(formatter.format(logrecord)).groupdict()
+            if self.request.session_hash == msg_obj['session_hash']:
+                del msg_obj['session_hash']
+                self.emit('log', msg_obj)
+                return True
+            else:
+                return False
 
-                gevent.sleep(0.1)
+        if existing_handler is None:
+            session_filter = logging.Filter()
+            session_filter.filter = emit_msg
 
-        self.spawn(send_logs)
+            session_log_folder = os.path.join(os.getcwd(), 'models\\session\\'+ self.request.session.session_id)
+            session_log_file = os.path.join(session_log_folder, self.request.session.session_id + '.log')
+            session_handler = logging.handlers.RotatingFileHandler(session_log_file,
+                                                                   mode='a',
+                                                                   maxBytes=1000000,
+                                                                   backupCount=3,
+                                                                   encoding=None,
+                                                                   delay=0)
+            session_handler.formatter = formatter
+            session_handler.session_id = self.request.session.session_id
+            overall_logger.info('handler for session {0} added'.format(self.request.session.session_id))
+            session_handler.addFilter(session_filter)
+
+            overall_logger.addHandler(session_handler)
+        else:
+            existing_handler.filters[0].filter = emit_msg
 
     def on_start_logger(self):
         print "Starting logger greenlet"
