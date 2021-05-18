8f03082faa7d8bdea8c68beb0a707eb0da0e0287
jay.hennen <jay.hennen@noaa.gov>
2021-05-18 16:21:15 -0700

    removed nesdisrelease for now

--- webgnome_api/views/release.py
+++ webgnome_api/views/release.py
@@ -45,12 +45,12 @@ release = Service(name='release', path='/release*obj_id',
 
 implemented_types = ('gnome.spill.release.PointLineRelease',
                      'gnome.spill.release.SpatialRelease',
-                     'gnome.spill.release.NESDISRelease',
+                     #'gnome.spill.release.NESDISRelease',
                      'gnome.spill.release.VerticalPlumeRelease',
                      )
 
 log = logging.getLogger(__name__)
-geojson_types = ('gnome.spill.release.SpatialRelease', 'gnome.spill.release.NESDISRelease')
+geojson_types = ('gnome.spill.release.SpatialRelease',)#, 'gnome.spill.release.NESDISRelease')
 
 @release.get()
 def get_release(request):
