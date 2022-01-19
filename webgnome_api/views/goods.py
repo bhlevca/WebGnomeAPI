9bd8f276d97a247a1eb182c0bc5a2320c8b851e3
amy.macfadyen <amy.macfadyen@noaa.gov>
2022-01-19 11:39:22 -0800

    Renaming

--- webgnome_api/views/goods.py
+++ webgnome_api/views/goods.py
@@ -29,14 +29,14 @@ import logging
 
 log = logging.getLogger(__name__)
 
-goods = Service(name='goods', path='/goods*',
+goods_maps = Service(name='maps', path='/goods/maps*',
                 description="GOODS API", cors_policy=cors_policy)
 
-goods_hycom = Service(name='goods_hycom', path='/goods_hycom*',
+goods_currents = Service(name='currents', path='/goods/currents*',
                       description="GOODS API", cors_policy=cors_policy)
 
 
-@goods.post()
+@goods_maps.post()
 def get_goods_map(request):
     '''
     Uses the payload passed by the client to make a .bna download request from GOODS.
@@ -52,8 +52,6 @@ def get_goods_map(request):
     #               'resolution': 'i',
     #               'submit': 'Get Map',
     #               }
-
-    # goods_resp = urllib.request.urlopen('https://gnome.orr.noaa.gov/goods/tools/GSHHS/coast_extract', request.body)
 
     params = request.POST
 
@@ -95,7 +93,7 @@ def get_goods_map(request):
 
     return file_path, file_name
 
-@goods_hycom.post()
+@goods_currents.post()
 def get_currrents(request):
     '''
     Uses the payload passed by the client to send information to libGOODS.
@@ -106,20 +104,28 @@ def get_currrents(request):
     switch_to_existing_session(request)
     upload_dir = os.path.relpath(get_session_dir(request))
     params = request.POST
-    fn, fp = currents.get_currents(model_name=params['model_name'],
-                            north_lat=float(params['NorthLat']),
-                            south_lat=float(params['SouthLat']),
-                            west_lon=float(params['WestLon']),
-                            east_lon=float(params['EastLon']),
-                            cross_dateline=bool(int(params['xDateline'])),
-                            )
-                            
-    file_name, unique_name = gen_unique_filename(fn, upload_dir)
-    
-    file_path = os.path.join(upload_dir, unique_name)
-    shutil.move(fp, file_path) # maybe I should pass session directory location to libgoods?
+    max_upload_size = eval(request.registry.settings['max_upload_size'])
+    try:
+        fn, fp = currents.get_currents(model_name=params['model_name'],
+                                north_lat=float(params['NorthLat']),
+                                south_lat=float(params['SouthLat']),
+                                west_lon=float(params['WestLon']),
+                                east_lon=float(params['EastLon']),
+                                cross_dateline=bool(int(params['xDateline'])),
+                                max_filesize=max_upload_size,
+                                )
+                       
+        file_name, unique_name = gen_unique_filename(fn, upload_dir)
         
-    log.info('Successfully uploaded file "{0}"'.format(file_path))
+        file_path = os.path.join(upload_dir, unique_name)
+        shutil.move(fp, file_path) # maybe I should pass session directory location to libgoods?
+            
+        log.info('Successfully uploaded file "{0}"'.format(file_path))
+        
+    except currents.FileTooBigError:
+            raise cors_response(request,
+                            HTTPBadRequest('file is too big!  Max size = {}'
+                                           .format(max_upload_size)))
 
     return file_path
 
