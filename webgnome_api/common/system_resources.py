085efdd0d2f01a9c22c5e1e835416879bd9c5767
James L. Makela <james.makela@noaa.gov>
2022-05-10 13:40:59 -0700

    Galveston Bay location wizard was failing to input offshore current (speed, direction)

--- webgnome_api/common/system_resources.py
+++ webgnome_api/common/system_resources.py
@@ -56,9 +56,11 @@ def write_fd_to_file(fd, out_path):
     # Set file to original position so we don't produce any side effects.
     fd.seek(curr_position)
 
+
 def write_bufread_to_file(bf, out_path):
     with open(out_path, 'wb') as output_file:
         output_file.writelines(bf.readlines())
+
 
 def mkdir(base_path, dir_name, mode=0o775):
     '''
@@ -141,9 +143,3 @@ def file_info(folder, f):
     return {'name': f,
             'size': size,
             'type': file_type}
-
-
-
-
-
-
